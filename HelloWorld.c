440524ed6d28c8cf0c3870896888ed99d18536ba
Olek <94283587+Oleeek@users.noreply.github.com>
2024-05-16 20:33:31 +0200

    Hello, World!

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+//
+// Created by lemsk on 16.05.2024.
+//
+// https://www.learn-c.org/en/Hello%2C_World!
+
+
+//exercise
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!");
+    return 0;
+}
+
+/* Output
+Hello, World!
+Process finished with exit code 0
+*/
+
